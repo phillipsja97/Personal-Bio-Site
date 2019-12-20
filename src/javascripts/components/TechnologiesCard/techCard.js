acefd2d2f552460c8abe17b05c7c3805129eb8aa
Jamie <phillipsja97@gmail.com>
2019-12-20 01:44:43 -0600

    tech is better

--- src/javascripts/components/TechnologiesCard/techCard.js
+++ src/javascripts/components/TechnologiesCard/techCard.js
@@ -3,19 +3,21 @@ import './techCard.scss';
 const techCard = (tech) => {
   let domString = '';
   domString += `
-  <div class="theCard"
-  <div class="d-flex flex-wrap justify-content-between">
-    <div class="card">
-      <img src="${tech.imageUrl}" class="card-img-top" alt="${tech.name}">
-      <div class="card-body">
-        <h5 class="card-title text-center">${tech.name}</h5>
-          <p class="card-text text-center">${tech.description}</p>
-      </div>
-    </div>
-  </div>
+  <div class="techImage">
+    <img src="${tech.imageUrl}" class="card-img-top" alt="${tech.name}">
   </div>
     `;
   return domString;
 };
 
 export default { techCard };
+
+// <div class="d-flex flex-wrap justify-content-between">
+//   <div class="card">
+//     <img src="${tech.imageUrl}" class="card-img-top" alt="${tech.name}">
+//     <div class="card-body">
+//       <h5 class="card-title text-center">${tech.name}</h5>
+//         <p class="card-text text-center">${tech.description}</p>
+//     </div>
+//   </div>
+// </div>
