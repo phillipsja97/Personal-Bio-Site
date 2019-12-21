f149ed1359237e26956a9ff7e1c51c0f2c4f4b57
Jamie <phillipsja97@gmail.com>
2019-12-20 22:02:55 -0600

    added some project styling

--- src/javascripts/components/projectCard/projectCard.js
+++ src/javascripts/components/projectCard/projectCard.js
@@ -3,18 +3,26 @@ import './projectCard.scss';
 const projectCard = (project) => {
   let domString = '';
   domString += `
-  <div class="projCard">
-  <div class="card" style="width: 18rem;">
-  <img src="${project.screenshot}" class="card-img-top" alt="${project.title}">
-  <div class="card-body">
-    <h5 class="card-title">${project.title}</h5>
-    <p class="card-text">${project.description}</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-  </div>
+  <div class="media" id="projCard">
+    <img src="${project.screenshot}" id="projImage" class="align-self-center mr-3" alt="${project.title}">
+      <div class="media-body text-center">
+        <h5 class="mt-0" id="cardTitle">${project.title}</h5>
+          <p>${project.description}</p>
+      </div>
   </div>
     `;
   return domString;
 };
 
 export default { projectCard };
+
+/* <div class="projCard">
+<div class="card" style="width: 18rem;">
+<img src="${project.screenshot}" class="card-img-top" alt="${project.title}">
+<div class="card-body">
+  <h5 class="card-title">${project.title}</h5>
+  <p class="card-text">${project.description}</p>
+  <a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+</div>
+</div> */
